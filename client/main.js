573571727d07673a678c6105c35a9ec18743be52
Hugh Willson <hugh@octonary.com>
2016-07-08 13:33:30 -0400

    Added proper font.

--- client/main.js
+++ client/main.js
@@ -6,9 +6,9 @@ import './main.html';
 
 Template.body.onRendered(function onRendered() {
   const plot = xkcdplot();
-  plot.width(400).height(300);
-  plot.title('Meteor ES2015 Modules - Q.E.D.');
-  plot.xlabel('Meteor ES2015 Module Usage');
+  plot.width(400).height(300)
+  plot.title('Meteor ES2015 Modules + npm - Q.E.D.');
+  plot.xlabel('Meteor ES2015 Module + npm Usage');
   const data = [];
   _.times(50, (index) => {
     data.push({ x: index, y: index });
