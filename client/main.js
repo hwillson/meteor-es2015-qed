b1f2aaab27d42571fd866e72ead61b09f9c77001
Hugh Willson <hugh@octonary.com>
2016-07-08 13:22:53 -0400

    The proof.

---
+++ client/main.js
@@ -0,0 +1,18 @@
+import { Template } from 'meteor/templating';
+import { _ } from 'meteor/underscore';
+import xkcdplot from 'xkcdplot';
+
+import './main.html';
+
+Template.body.onRendered(function onRendered() {
+  const plot = xkcdplot();
+  plot.width(400).height(300);
+  plot.title('Meteor ES2015 Modules - Q.E.D.');
+  plot.xlabel('Meteor ES2015 Module Usage');
+  const data = [];
+  _.times(50, (index) => {
+    data.push({ x: index, y: index });
+  });
+  plot.plot(data, {stroke: "red"});
+  plot('body').xlim([-2, 50]).ylim([-2, 50]).draw();
+});
